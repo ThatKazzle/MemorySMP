3037d246b116d73a2e16188840c2c36f487d5717
Kazzle-Laptop <thatkazzle@gmail.com>
2024-12-01 19:26:53 -0600

    Update 1.4.1
    - added test cooldown display for the locked membrane

--- src/main/java/org/kazzleinc/memorySMP/membranes/LockedMembraneClass.java
+++ src/main/java/org/kazzleinc/memorySMP/membranes/LockedMembraneClass.java
@@ -1,5 +1,6 @@
 package org.kazzleinc.memorySMP.membranes;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.kazzleinc.memorySMP.MemorySMP;
@@ -13,16 +14,16 @@ public class LockedMembraneClass extends ParentMembraneClass implements Listener
 
     @Override
     public void action1(Player player) {
-
+        player.sendMessage("AH ! ACTION ONE !");
     }
 
     @Override
     public void action2(Player player) {
-
+        player.sendMessage("AH ! ACTION TWO !");
     }
 
     @Override
     public void cooldownDisplay(Player player) {
-
+        player.sendActionBar(ChatColor.GREEN + "Cooldown 1: " + ChatColor.RESET + ChatColor.BOLD + ChatColor.RED + "NOT IMP" + ChatColor.RESET + ChatColor.BOLD + ChatColor.GOLD + " | " + ChatColor.RESET + ChatColor.GREEN + "Cooldown 2: " + ChatColor.RESET + ChatColor.RED + ChatColor.BOLD + "NOT IMP");
     }
 }
